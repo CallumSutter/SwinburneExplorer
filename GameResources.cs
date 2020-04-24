cf0481fa4294f4d3940a9d3a1c3feed0163b1ffc
Olodhin <icelasersparr@gmail.com>
2020-04-24 17:36:43 +1000

    Adding GameResources class

---
+++ GameResources.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SplashKitSDK;
+
+namespace Swinburneexplorer
+{
+	public static class GameResources
+	{
+		private static Dictionary<string, Font> _fonts;
+		private static Dictionary<string, SoundEffect> _sounds;
+		private static Dictionary<string, Bitmap> _images;
+		private static Dictionary<string, Music> _music;
+
+		static GameResources()
+		{
+			LoadImages();
+			LoadFonts();
+			LoadMusic();
+			LoadSounds();
+		}
+
+		private static void LoadingScreen()
+		{
+			
+		}
+
+		private static void LoadImages()
+		{
+
+		}
+
+		private static void LoadSounds()
+		{
+
+		}
+
+		private static void LoadMusic()
+		{
+			_music.Add("BGM", new Music("BGM", "\\Resources\\Music\\GN.mp3"));
+		}
+
+		private static void LoadFonts()
+		{
+
+		}
+
+		public static Font GetFont(string fontName)
+		{
+			try
+			{
+				return _fonts[fontName];
+			}
+			catch (KeyNotFoundException e)
+			{
+				Console.WriteLine("Cannot find font: " + fontName);
+				Console.WriteLine(e.Message);
+			}
+
+			return null;
+		}
+
+		public static SoundEffect GetSound(string soundName)
+		{
+			try
+			{
+				return _sounds[soundName];
+			}
+			catch (KeyNotFoundException e)
+			{
+				Console.WriteLine("Cannot find sound effect: " + soundName);
+				Console.WriteLine(e.Message);
+			}
+			
+			return null;
+		}
+
+		public static Music GetMusic(string musicName)
+		{
+			try
+			{
+				return _music[musicName];
+			}
+			catch (KeyNotFoundException e)
+			{
+				Console.WriteLine("Cannot find music: " + musicName);
+				Console.WriteLine(e.Message);
+			}
+
+			return null;
+		}
+
+		public static Bitmap GetImage(string imgName)
+		{
+			try
+			{
+				return _images[imgName];
+			}
+			catch (KeyNotFoundException e)
+			{
+				Console.WriteLine("Cannot find font: " + imgName);
+				Console.WriteLine(e.Message);
+			}
+
+			return null;
+		}
+	}
+}
