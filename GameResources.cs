4219e0e01a30ae016f6e238a0a69810a3849c4fc
Olodhin <icelasersparr@gmail.com>
2020-04-26 19:37:14 +1000

    Add features to GameResources

--- GameResources.cs
+++ GameResources.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using SplashKitSDK;
 
-namespace Swinburneexplorer
+namespace Resources
 {
 	public static class GameResources
 	{
@@ -12,37 +12,89 @@ namespace Swinburneexplorer
 		private static Dictionary<string, Bitmap> _images;
 		private static Dictionary<string, Music> _music;
 
+		private static Font _loadingFont;
+
 		static GameResources()
 		{
-			LoadImages();
+			_fonts = new Dictionary<string, Font>();
+			_sounds = new Dictionary<string, SoundEffect>();
+			_images = new Dictionary<string, Bitmap>();
+			_music = new Dictionary<string, Music>();
+			_loadingFont = new Font("arial", "/arial.ttf");
+		}
+
+		public static void PlayBGM()
+		{
+			if(SplashKit.MusicPlaying())
+			{
+				SplashKit.StopMusic();
+			}
+
+			SplashKit.PlayMusic(GetMusic("bgm"));
+		}
+
+		public static void LoadingScreen()
+		{
+			Rectangle loadbar = new Rectangle();
+			loadbar.Height = 13;
+			loadbar.Width = 100;
+			loadbar.X = Program.WINDOW_WIDTH / 2;
+			loadbar.Y = Program.WINDOW_HEIGHT / 2;
+
+			SplashKit.Delay(250);
+			Program.gameWindow.FillRectangle(Color.LimeGreen ,loadbar);
+			Program.gameWindow.Refresh();
 			LoadFonts();
+
+			loadbar.X += loadbar.Width;
+
+			SplashKit.Delay(250);
+			Program.gameWindow.FillRectangle(Color.LimeGreen, loadbar);
+			Program.gameWindow.Refresh();
+			LoadImages();
+
+			loadbar.X += loadbar.Width;
+
+			SplashKit.Delay(250);
+			Program.gameWindow.FillRectangle(Color.LimeGreen, loadbar);
+			Program.gameWindow.Refresh();
 			LoadMusic();
+
+			loadbar.X += loadbar.Width;
+
+			SplashKit.Delay(250);
+			Program.gameWindow.FillRectangle(Color.LimeGreen, loadbar);
+			Program.gameWindow.Refresh();
 			LoadSounds();
 		}
 
-		private static void LoadingScreen()
+		private static void FreeResources()
 		{
-			
+			SplashKit.FreeAllBitmaps();
+			SplashKit.FreeAllFonts();
+			SplashKit.FreeAllMusic();
+			SplashKit.FreeAllSoundEffects();
+			SplashKit.ProcessEvents();
 		}
 
 		private static void LoadImages()
 		{
-
+			//_images.Add("loadbar", new Bitmap("bmp", "path"));
 		}
 
 		private static void LoadSounds()
 		{
-
+			//_sounds.Add("soundName", new SoundEffect("soundName", "Path"));
 		}
 
 		private static void LoadMusic()
 		{
-			_music.Add("BGM", new Music("BGM", "\\Resources\\Music\\GN.mp3"));
+			_music.Add("bgm", new Music("bgm", "sogno.mp3"));
 		}
 
 		private static void LoadFonts()
 		{
-
+			//_fonts.Add("fontName", new Font("fontName", "Path"));
 		}
 
 		public static Font GetFont(string fontName)
--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
 using System;
 using SplashKitSDK;
+using Resources;
 
 public class Program
 {
+	public const int WINDOW_WIDTH = 1920;
+	public const int WINDOW_HEIGHT = 1080;
+
+	public static Window gameWindow = new Window("Swinburne Explorer", WINDOW_WIDTH, WINDOW_HEIGHT);
+
     public static void Main(string[] args)
     {
-        Window shapesWindow;
-        shapesWindow = new Window("Shapes by ...", 800, 600);
+		gameWindow.MoveTo(0, 0);
 
-        shapesWindow.Clear(Color.White);
-        shapesWindow.FillEllipse(Color.BrightGreen, 0, 400, 800, 400);
-        shapesWindow.FillRectangle(Color.Gray, 300, 300, 200, 200);
-        shapesWindow.FillTriangle(Color.Red, 250, 300, 400, 150, 550, 300);
-        shapesWindow.Refresh();
+		gameWindow.Clear(Color.White);
+		gameWindow.FillEllipse(Color.BrightGreen, 0, 400, 800, 400);
+		gameWindow.FillRectangle(Color.Gray, 300, 300, 200, 200);
+		gameWindow.FillTriangle(Color.Red, 250, 300, 400, 150, 550, 300);
+		gameWindow.Refresh();
 
-        SplashKit.Delay(5000);
+		GameResources.LoadingScreen();
+		GameResources.PlayBGM();
+
+		Console.ReadLine();
     }
 }
