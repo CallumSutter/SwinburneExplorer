39f710a6000f0444d73c05878a7b118ecb9177ff
Olodhin <icelasersparr@gmail.com>
2020-05-01 01:57:12 +1000

    Add loadscreen basics, arrow overlay and arrow click detection

--- Program.cs
+++ Program.cs
@@ -4,24 +4,37 @@ using Resources;
 
 public class Program
 {
-	public const int WINDOW_WIDTH = 1920;
-	public const int WINDOW_HEIGHT = 1080;
+	public const int WINDOW_WIDTH = 800;
+	public const int WINDOW_HEIGHT = 600;
+
+	public const int TARGET_FPS = 60;
+	public const uint DELAY = (uint)(1.0 / TARGET_FPS * 1000);
 
 	public static Window gameWindow = new Window("Swinburne Explorer", WINDOW_WIDTH, WINDOW_HEIGHT);
 
     public static void Main(string[] args)
     {
-		gameWindow.MoveTo(0, 0);
-
-		gameWindow.Clear(Color.White);
-		gameWindow.FillEllipse(Color.BrightGreen, 0, 400, 800, 400);
-		gameWindow.FillRectangle(Color.Gray, 300, 300, 200, 200);
-		gameWindow.FillTriangle(Color.Red, 250, 300, 400, 150, 550, 300);
-		gameWindow.Refresh();
+		//gameWindow.Clear(Color.White);
+		//gameWindow.FillEllipse(Color.BrightGreen, 0, 400, 800, 400);
+		//gameWindow.FillRectangle(Color.Gray, 300, 300, 200, 200);
+		//gameWindow.FillTriangle(Color.Red, 250, 300, 400, 150, 550, 300);
+		//gameWindow.Refresh();
 
 		GameResources.LoadingScreen();
-		GameResources.PlayBGM();
+		//GameResources.PlayBGM();
 
-		Console.ReadLine();
+		while(!gameWindow.CloseRequested)
+		{
+			SplashKit.ProcessEvents();
+
+			SplashKit.Delay(DELAY);
+
+			if (SplashKit.MouseClicked(MouseButton.LeftButton))
+			{
+				Console.WriteLine(GameResources.MouseInArrow().ToString());
+			}
+		}
+
+		gameWindow.Close();
     }
 }
