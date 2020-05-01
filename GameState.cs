eedfdfd857d9ed6e32185b034f7a3ecd7634d783
tahsinb <54355406+tahsinb@users.noreply.github.com>
2020-05-01 16:15:24 +1000

    Add files via upload

---
+++ GameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SplashKitSDK;
+
+namespace Swinburneexplorer
+{
+    public enum GameState
+    {
+        //Player is at primary location
+        startLocation,
+
+        //Player is moving between locations
+        Travelling,
+
+        //Player has arrived at destination
+        finalDestination,
+    }
+}
