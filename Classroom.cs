1720bbada276ce41d3b74d4920cb934183fdbd72
tahsinb <54355406+tahsinb@users.noreply.github.com>
2020-05-08 15:32:53 +1000

    building.cs and classroom.cs

---
+++ Classroom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swinburneexplorer
+{
+    public class Classroom
+    {
+        private string _roomId;
+
+        public Classroom(string roomId)
+        { }
+
+        public string RoomId
+        {
+            get { return _roomId; }
+        }
+    }
+}
