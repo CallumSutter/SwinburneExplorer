49223e89ed4398699c526f39f7de470df68d41eb
Olodhin <icelasersparr@gmail.com>
2020-05-09 21:57:03 +1000

    Add unit test

---
+++ BuildingTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SplashKitSDK;
+
+namespace Swinburneexplorer
+{
+	public class BuildingTest
+	{
+		Location l1;
+		Location l2;
+		Location l3;
+		Location l4;
+		Building b1;
+		Building b2;
+
+		[SetUp]
+		public void SetUp() {
+			// initialise locations
+			l1 = new Location("l1", "l1Info");
+			l2 = new Location("l2");
+			l3 = new Location("l3");
+			l4 = new Location("l4");
+
+			// initialise building
+			b1 = new Building("b1");
+			b2 = new Building("b2");
+
+			// add connecting locations from l1
+			l1.AddConnectingLocation(l2, 0);
+			l1.AddConnectingLocation(l3, 1);
+			l1.AddConnectingLocation(l4, 2);
+
+			// set b1 as L1's building
+			l1.SetBuilding(b1);
+		}
+
+		[Test]
+		public void BuildingInLocation() {
+			// enter building from l1
+			Assert.AreEqual(b1, l1.EnterBuilding);
+		}
+
+		[Test]
+		public void NoBuildingInLocation() {
+			// fail enter if no building exists
+			Assert.IsNull(l2.EnterBuilding);
+		}
+
+		[Test]
+		public void ExitBuilding_Success() {
+			// get original location from building
+			Assert.AreEqual(l1, b1.ParentLoc);
+		}
+
+		[Test]
+		public void BuildingInformation_Success()	{
+			// get original location info from building's location
+			Assert.AreEqual("l1Info", b1.GetInfo);
+		}
+
+		[Test]
+		public void BuildingInformation_None() {
+			// no info should return null
+			Assert.IsNull(b2.GetInfo);
+		}
+	}
+}
---
+++ UIObjectTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using SplashKitSDK;
+
+namespace Swinburneexplorer
+{
+	public class UIObjectTests
+	{
+		// object drawn onto UI
+		UIObject buildingIcon;
+		Rectangle buildingIconMask;
+
+		[SetUp]
+		public void Setup()
+		{
+			// mask for UI object
+			buildingIconMask = new Rectangle();
+			buildingIconMask.X = 0;
+			buildingIconMask.Y = 0;
+			buildingIconMask.Height = 10;
+			buildingIconMask.Width = 10;
+
+			// UIObject constructor has two parameters: Image, and mask for image
+			buildingIcon = new UIObject(/*Object image,*/ buildingIconMask);
+			buildingIcon.Visible = true;
+		}
+
+		[TestCase(0,0)]
+		[TestCase(10, 0)]
+		[TestCase(0, 10)]
+		[TestCase(10, 10)]
+		[TestCase(5, 5)]
+		public void MouseInIcon_IsTrue(double x, double y)
+		{
+			Point2D mousePosition = new Point2D();
+			mousePosition.X = x;
+			mousePosition.Y = y;
+
+			Assert.IsTrue(buildingIcon.IsHovering(mousePosition));
+		}
+
+		[TestCase(-1, -1)]
+		[TestCase(11, -1)]
+		[TestCase(-1, 11)]
+		[TestCase(11, 11)]
+		[TestCase(11, 5)]
+		public void MouseInIcon_IsFalse(double X, double Y)
+		{
+			Point2D mousePosition = new Point2D();
+			mousePosition.X = X;
+			mousePosition.Y = Y;
+
+			Assert.IsFalse(buildingIcon.IsHovering(mousePosition));
+		}
+	}
+}
